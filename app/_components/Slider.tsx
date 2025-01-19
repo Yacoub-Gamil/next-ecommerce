661c7c268653fdb8f0fea7666591158d33847060
Yacoub-Gamil <yacoub-gamil@hotmail.com>
2025-01-19 15:55:51 +0200

    added the slider for home page

--- app/_components/Slider.tsx
+++ app/_components/Slider.tsx
@@ -1,7 +1,7 @@
 "use client";
 import Image from "next/image";
 import Link from "next/link";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const slider = [
   {
@@ -10,7 +10,7 @@ const slider = [
     description: "Sale! Up to 50%  off! ",
     url: "/",
     image:
-      "https://images.pexels.com/photos/30218480/pexels-photo-30218480/free-photo-of-charming-alleyway-market-with-textiles.jpeg?auto=compress&cs=tinysrgb&w=1200&lazy=load",
+      "https://images.pexels.com/photos/1689731/pexels-photo-1689731.jpeg?auto=compress&cs=tinysrgb&w=1200",
     bg: "bg-gradient-to-r from-pink-50 to-blue-50",
   },
   {
@@ -19,7 +19,7 @@ const slider = [
     description: "Sale! Up to 50%  off! ",
     url: "/",
     image:
-      "https://images.pexels.com/photos/30218480/pexels-photo-30218480/free-photo-of-charming-alleyway-market-with-textiles.jpeg?auto=compress&cs=tinysrgb&w=1200&lazy=load",
+      "https://images.pexels.com/photos/157675/fashion-men-s-individuality-black-and-white-157675.jpeg?auto=compress&cs=tinysrgb&w=1200",
     bg: "bg-gradient-to-r from-pink-50 to-blue-50",
   },
   {
@@ -28,7 +28,7 @@ const slider = [
     description: "Sale! Up to 50% off! ",
     url: "/",
     image:
-      "https://images.pexels.com/photos/30218480/pexels-photo-30218480/free-photo-of-charming-alleyway-market-with-textiles.jpeg?auto=compress&cs=tinysrgb&w=1200&lazy=load",
+      "https://images.pexels.com/photos/818992/pexels-photo-818992.jpeg?auto=compress&cs=tinysrgb&w=1200",
     bg: "bg-gradient-to-r from-pink-50 to-blue-50",
   },
 ];
@@ -36,9 +36,19 @@ const slider = [
 export default function Slider() {
   const [current, setCurrent] = useState(0);
 
+  useEffect(() => {
+    const slide = setInterval(() => {
+      setCurrent((cur) => (cur === slider.length - 1 ? 0 : cur + 1));
+    }, 10000);
+    return () => clearInterval(slide);
+  }, []);
+
   return (
-    <div className=" h-[calc(100vh-80px)] overflow-hidden">
-      <div className=" w-max h-full flex transition-all ease-in-out duration-1000">
+    <div className=" h-[calc(100vh-120px)] overflow-hidden">
+      <div
+        className=" w-max h-full flex transition-all ease-in-out duration-1000"
+        style={{ transform: `translateX(-${current * 100}vw` }}
+      >
         {slider.map((slide) => (
           <div
             className={`${slide.bg} w-screen h-full flex flex-col gap-16
@@ -61,15 +71,24 @@ export default function Slider() {
               </Link>
             </div>
             {/* IMAGE CONTAINER  */}
-            <div className=" h-1/2 xl:w-1/2 xl:h-full relative">
-              <Image
-                src={slide.image}
-                alt={slide.image}
-                fill
-                sizes="100%"
-                className=" object-cover"
-              />
+            <div className=" h-1/2 xl:w-1/2 xl:h-full aspect-square relative">
+              <Image src={slide.image} alt={slide.image} fill sizes="100%" />
             </div>
+          </div>
+        ))}
+      </div>
+      <div className=" absolute m-auto left-1/2 flex bottom-12 gap-4">
+        {slider.map((slide, index) => (
+          <div
+            key={slide.id}
+            className={` w-3 h-3 rounded-full ring-1 ring-gray-600 cursor-pointer flex items-center justify-center ${
+              current === index ? "scale-150" : ""
+            }`}
+            onClick={() => setCurrent(index)}
+          >
+            {current === index && (
+              <div className="w-[6px] h-[6px] bg-gray-600 rounded-full transition-all duration-700"></div>
+            )}
           </div>
         ))}
       </div>
